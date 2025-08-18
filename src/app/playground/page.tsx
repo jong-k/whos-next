d081273342314fdcadfeb9fe2aeacdbb9a4a6adf
jong-k <fully.charged07@gmail.com>
2025-08-18 13:26:18 +0900

    feat: transition pending 상태 추가

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,42 +1,47 @@
 "use client";
-import { useOptimistic, useState, startTransition } from "react";
+import { useOptimistic, useState, useTransition } from "react";
 import { Button } from "@/components/ui/button";
 
 export default function PlayGroundPage() {
-  const [count, setCount] = useState(0);
-  const [optimisticCount, addOptimisticCount] = useOptimistic(
+  const [count, setCount] = useState<number>(0);
+  const [error, setError] = useState<string | null>(null);
+  const [isPending, startUiTransition] = useTransition();
+  const [optimisticCount, addOptimisticCount] = useOptimistic<number, number>(
     count,
-    (state, increment: number) => {
+    (state, increment) => {
       return state + increment;
     }
   );
 
   const handleClick = () => {
-    startTransition(async () => {
-      addOptimisticCount(1);
+    startUiTransition(async () => {
+      setError(null);
+      addOptimisticCount(1); // 낙관적 업데이트
+
       try {
         await new Promise<void>(res => setTimeout(() => res(), 1500));
         const isSuccess = Math.round(Math.random()) === 0;
-        if (!isSuccess) {
-          throw new Error("increment failed!");
-        } else {
-          console.log("increment success!");
-        }
+        if (!isSuccess) throw new Error("increment failed!");
         setCount(prev => prev + 1);
       } catch (err) {
-        console.error(err);
+        setError((err as Error).message);
+        // 실패 시 optimisticCount가 count로 자동 롤백
       }
     });
   };
 
   return (
     <div>
-      <h2 className="text-2xl">Hello from Playground</h2>
-      <div className="space-x-2">
+      <h2 className="mb-4 text-2xl">Hello from Playground</h2>
+      <h3 className="text-xl">낙관적 카운터</h3>
+      <div className="flex items-center gap-4">
         <span>count: {count}</span>
         <span>optimisticCount: {optimisticCount}</span>
-        <Button onClick={handleClick}>click me</Button>
+        <Button onClick={handleClick} disabled={isPending}>
+          {isPending ? "Updating..." : "click me"}
+        </Button>
       </div>
+      {error && <p className="text-sm text-red-500">{error}</p>}
     </div>
   );
 }
