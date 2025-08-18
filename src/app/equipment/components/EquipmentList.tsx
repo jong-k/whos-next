291a0d49cfa66ac1d629cf26d8aee9618d0ea995
jong-k <fully.charged07@gmail.com>
2025-08-18 18:30:33 +0900

    feat: list를 테이블 형태로 변경

--- src/app/equipment/components/EquipmentList.tsx
+++ src/app/equipment/components/EquipmentList.tsx
@@ -12,14 +12,28 @@ export default function EquipmentList() {
   if (error) return <div>에러가 발생했습니다</div>;
 
   return (
-    <ul>
-      {data!.map(equipment => (
-        <li key={equipment.id}>
-          <div>{equipment.model}</div>
-          <div>{equipment.serial}</div>
-          <div>{equipment.disabled ? "true" : "false"}</div>
-        </li>
-      ))}
-    </ul>
+    <table className="w-full">
+      <thead>
+        <tr className="border-b">
+          <th className="p-4 text-left">시리얼번호</th>
+          <th className="p-4 text-left">모델</th>
+          <th className="p-4 text-left">상태</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data!.map((equipment, index) => (
+          <tr
+            key={equipment.id}
+            className={`hover:bg-gray-50 ${index !== data!.length - 1 ? "border-b" : ""}`}
+          >
+            <td className="p-4">{equipment.serial}</td>
+            <td className="p-4">{equipment.model}</td>
+            <td className="p-4">
+              {equipment.disabled ? "비활성화" : "활성화"}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
