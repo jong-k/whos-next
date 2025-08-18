ba4de3acb9ba01109ad838826b7f2029b9c98951
jong-k <fully.charged07@gmail.com>
2025-08-18 21:56:19 +0900

    refactor: route 경로 상수화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import { ROUTES } from "@/constants/routes";
 
 export default function Home() {
   return (
-    <ul>
+    <ul className="flex flex-col gap-2">
       <li>
-        <Link href="/equipment">장비 상태 변경 mutation 최적화</Link>
-        <Link href="/optimistic-counter">낙관적 카운터 UI</Link>
+        <Link href={ROUTES.equipment}>장비 상태 변경 mutation 최적화</Link>
+      </li>
+      <li>
+        <Link href={ROUTES.optimisticCounter}>낙관적 카운터 UI</Link>
       </li>
     </ul>
   );
---
+++ src/constants/routes.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+  equipment: "/equipment",
+  optimisticCounter: "/optimistic-counter",
+};
