63b321aa372bb26bd71599b6aefac45f1b8ce2a7
jong-k <fully.charged07@gmail.com>
2025-08-20 15:11:54 +0900

    chore: storybook 세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,5 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -14,6 +16,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...compat.extends("plugin:prettier/recommended"),
   ...pluginQuery.configs["flat/recommended"],
+  ...storybook.configs["flat/recommended"],
 ];
 
 export default eslintConfig;
